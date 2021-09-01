607e05f9943b0ea34eb46e8e0f7c454680d787b0
Oladeji Sulaiman <oladejisulaiman@yahoo.com>
2021-09-01 14:44:57 -0400

    Add project files.

---
+++ FizzBuzz/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace FizzBuzz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = new int[100];
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = i + 1;
+            }
+            FizzBuzz(array);
+        }
+
+        public static void FizzBuzz(int[] array)
+        {
+            string output;
+            for (int i = 0; i < array.Length; i++)
+            {
+                output = "";
+                if (array[i] % 3 == 0 || array[i] % 5 == 0)
+                {
+                    if (array[i] % 3 == 0)
+                    {
+                        output = "fizz";
+
+
+                    }
+                    if (array[i] % 5 == 0)
+                    {
+                        output += "buzz";
+
+                    }
+                    Console.WriteLine(output);
+                }
+
+
+
+            }
+
+
+
+        }
+    }
+}
